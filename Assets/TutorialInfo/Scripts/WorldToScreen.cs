8c11657ebe0c5ccacdf34598dd8883195bfc5ae4
Quangbikini <153468332+quangpai290905@users.noreply.github.com>
2026-01-14 17:10:16 +0700

    Add files via upload

---
+++ Assets/TutorialInfo/Scripts/WorldToScreen.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+public class WorldToScreen : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 screenPos =
+            Camera.main.WorldToScreenPoint(transform.position);
+        Debug.Log(screenPos);
+    }
+}
